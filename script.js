94617b98c305ec079f14c3f0b66874eafb2ca38a
Adriana Coll <169408160+Techalotl@users.noreply.github.com>
2024-08-21 16:28:15 -0600

    Change logic for the game
    The buttons now do things!
    The game is fully playable but there is not UI yet.

--- script.js
+++ script.js
@@ -1,70 +1,59 @@
 function getComputerChoice() {
     let random = Math.floor(Math.random()*3);
     if (random === 0) {
-        return "rock"
+        return "Rock"
     } else if (random === 1) {
-        return "paper"
+        return "Paper"
     } else {
-        return "scissors"
+        return "Scissors"
     }
 }
 
-function getHumanChoice() {
-    let choice = prompt("Rock, paper or scissors?")
-    return choice.toLowerCase();
-}
+const rockButton = document.querySelector("#rock-button");
+const paperButton = document.querySelector("#paper-button");
+const scissorsButton = document.querySelector("#scissors-button");
 
-function playGame() {
-    let humanScore = 0
-    let computerScore = 0
-    function playRound(humanChoice, computerChoice) {
-        if (humanChoice === computerChoice) {
-            console.log("You chose " + humanChoice, "and the computer chose " + computerChoice)
-            console.log("It's a tie!");
-            console.log(humanScore, computerScore)
-        } else if (humanChoice === "rock" && computerChoice === "scissors") {
-            console.log("You chose " + humanChoice, "and the computer chose " + computerChoice)
-            console.log("You win this round! Rock beats Scissors")
-            humanScore++
-            console.log(humanScore, computerScore)
-        } else if (humanChoice === "paper" && computerChoice === "rock") {
-            console.log("You chose " + humanChoice, "and the computer chose " + computerChoice)
-            console.log("You win this round! Paper beats Rock")
-            humanScore++
-            console.log(humanScore, computerScore)
-        } else if (humanChoice === "scissors" && computerChoice === "paper") {
-            console.log("You chose " + humanChoice, "and the computer chose " + computerChoice)
-            console.log("You win this round! Scissors beats Paper")
-            humanScore++
-            console.log(humanScore, computerScore)
-        } else if (humanChoice === "rock" && computerChoice === "paper") {
-            console.log("You chose " + humanChoice, "and the computer chose " + computerChoice)
-            console.log("You lose this round! :( Paper beats Rock")
-            computerScore++
-            console.log(humanScore, computerScore)
-        } else if (humanChoice === "paper" && computerChoice === "scissors") {
-            console.log("You chose " + humanChoice, "and the computer chose " + computerChoice)
-            console.log("You lose this round! :( Scissors beats Paper");
-            computerScore++
-            console.log(humanScore, computerScore)
-        } else {
-            console.log("You chose " + humanChoice, "and the computer chose " + computerChoice)
-            console.log("You lose this round! :( Rock beats Scissors")
-            computerScore++
-            console.log(humanScore, computerScore)
-        }
+rockButton.addEventListener("click", () => {
+    playRound("Rock", getComputerChoice());
+});
+paperButton.addEventListener("click", () => {
+    playRound("Paper", getComputerChoice());
+});
+scissorsButton.addEventListener("click", () => {
+    playRound("Scissors", getComputerChoice());
+});
+
+let humanScore = 0
+let computerScore = 0
+    
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log("You chose " + humanChoice, "and the computer chose " + computerChoice)
+        console.log("It's a tie!");
+        console.log(humanScore, computerScore)
+    } else if ((humanChoice === "Rock" && computerChoice === "Scissors") ||
+                (humanChoice === "Paper" && computerChoice === "Rock") ||
+                (humanChoice === "Scissors" && computerChoice === "Paper")) {
+        console.log("You chose " + humanChoice, "and the computer chose " + computerChoice)
+        console.log("You win this round! " + humanChoice + " beats " + computerChoice)
+        humanScore++
+        console.log(humanScore, computerScore)
+    } else {
+        console.log("You chose " + humanChoice, "and the computer chose " + computerChoice)
+        console.log("You lose this round! :( " + computerChoice + " beats " + humanChoice);
+        computerScore++;
+        console.log(humanScore, computerScore)
     }
-    playRound(humanSelection, computerSelection);
+    if (humanScore === 5 || computerScore === 5) {
+    rockButton.disabled = true;
+    paperButton.disabled = true;
+    scissorsButton.disabled = true;
     if (humanScore > computerScore) {
         console.log("Congratulations! You win the game")
     } else if (humanScore === computerScore) {
         console.log("Tie ¯\_(ツ)_/¯")
-    }else {
+    } else {
         console.log("You lose the game. F")
     }
 }
-
-let humanSelection = getHumanChoice();
-let computerSelection = getComputerChoice();
-
-playGame();
+}
